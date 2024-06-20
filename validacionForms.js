11aa8b79ef50138773999ff21fb0942b891e6804
DiegoMarchandon <diegomarc2020@gmail.com>
2024-06-20 00:50:42 -0300

    form detalles

--- validacionForms.js
+++ validacionForms.js
@@ -1,2 +1,10 @@
 // página para realizar la validacion de formulario de ingreso a la pagina web
 
+function mostrarForm(){
+    // alert("fui clickeado");
+    /* utilizar ByTagName para el <u> y le agregamos el id a la etiqueta p que vamos a ocultar también. */
+    var nuevoUsuario = document.getElementById("nuevoUsuario");
+    var registrarse = document.getElementById("registro");
+    nuevoUsuario.style.display = 'block';
+    registrarse.style.display = 'none';
+}
