ec8c1d6cada87278e9a4636f92378bdb4e59e065
DiegoMarchandon <diegomarc2020@gmail.com>
2024-06-21 00:52:03 -0300

    cositas

--- validacionForms.js
+++ validacionForms.js
@@ -5,6 +5,15 @@ function mostrarForm(){
     /* utilizar ByTagName para el <u> y le agregamos el id a la etiqueta p que vamos a ocultar también. */
     var nuevoUsuario = document.getElementById("nuevoUsuario");
     var registrarse = document.getElementById("registro");
+    var parrafoSoyNuevo = document.getElementById("imNew");
     nuevoUsuario.style.display = 'block';
     registrarse.style.display = 'none';
+    parrafoSoyNuevo.style.display = 'none';
+
+    /* manualmente, me aseguro de que todos los estilos se apliquen .*/
+    // var formulario = document.getElementsByTagName("form")[1];
+    /* la coleccion devuelta no es un array real sino un objeto de tipo
+    'HTML collection'. Puedo convertirla a coleccion usando Array.from */
+    nuevoUsuario.style.marginTop = '50px';
+    nuevoUsuario.style.textAlign = 'center';
 }
