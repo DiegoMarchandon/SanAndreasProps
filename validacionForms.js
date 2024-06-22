a7cca07fec8fc2dc152ae99465a3cff545f56fe8
DiegoMarchandon <diegomarc2020@gmail.com>
2024-06-21 21:30:51 -0300

    mapa funcional

--- validacionForms.js
+++ validacionForms.js
@@ -2,11 +2,10 @@
 
 function mostrarForm(){
     // alert("fui clickeado");
-    /* utilizar ByTagName para el <u> y le agregamos el id a la etiqueta p que vamos a ocultar también. */
-    var nuevoUsuario = document.getElementById("nuevoUsuario");
-    var registrarse = document.getElementById("registro");
+    var registro = document.getElementById("registro");
+    var registrarse = document.getElementById("ingreso");
     var parrafoSoyNuevo = document.getElementById("imNew");
-    nuevoUsuario.style.display = 'block';
+    registro.style.display = 'block';
     registrarse.style.display = 'none';
     parrafoSoyNuevo.style.display = 'none';
     document.getElementById('box-form').style.height = '500px';
@@ -14,6 +13,6 @@ function mostrarForm(){
     // var formulario = document.getElementsByTagName("form")[1];
     /* la coleccion devuelta no es un array real sino un objeto de tipo
     'HTML collection'. Puedo convertirla a coleccion usando Array.from */
-    nuevoUsuario.style.marginTop = '50px';
-    nuevoUsuario.style.textAlign = 'center';
+    registro.style.marginTop = '50px';
+    registro.style.textAlign = 'center';
 }
