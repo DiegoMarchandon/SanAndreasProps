767357323f9624de97823a65bb01a572791df23f
DiegoMarchandon <diegomarc2020@gmail.com>
2024-07-01 02:04:31 -0300

    asd

--- slidesJS.js
+++ slidesJS.js
@@ -5,3 +5,15 @@ let coleccionIMG = [
     'imagenes/slider/vista.jpg'
 ];
 
+let actualIndex = 0;
+
+function cambioImagen(direccion){
+    var contenedorIMG = document.getElementsByClassName("img-container")[0];
+    if(direccion == 'siguiente'){
+        // aplico el resto % para que en caso de llegar al final, se pueda reiniciar
+        actualIndex = (actualIndex +1) % coleccionIMG.length;
+    }else if(direccion == 'anterior'){
+        actualIndex = (actualIndex -1 + coleccionIMG.length) % coleccionIMG.length;
+    }
+    contenedorIMG.style.backgroundImage = 'url('+coleccionIMG[actualIndex]+')';
+}
