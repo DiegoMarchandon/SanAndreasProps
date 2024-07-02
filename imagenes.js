f0afe5904e39ecd4791d156a70397e5c39815914
DiegoMarchandon <diegomarc2020@gmail.com>
2024-07-01 22:55:35 -0300

    imagenes y arreglos cargados

--- imagenes.js
+++ imagenes.js
@@ -1,85 +1,201 @@
 
-var Casa1LS = [
-    'imagenes\slider\casasLS\gallery32Casa1LS.jpg',
-    'imagenes\slider\casasLS\gallery24.jpg',
-    'imagenes\slider\casasLS\gallery25.jpg',
-    'imagenes\slider\casasLS\gallery26.jpg',
-    'imagenes\slider\casasLS\gallery27.jpg',
-    'imagenes\slider\casasLS\gallery28.jpg',
-    'imagenes\slider\casasLS\gallery30.jpg',
-    'imagenes\slider\casasLS\gallery31.jpg'
+var Casa1LSfotos = [
+    'imagenes\slider\casasFotos\gallery32Casa1LS.jpg',
+    'imagenes\slider\casasFotos\gallery24.jpg',
+    'imagenes\slider\casasFotos\gallery25.jpg',
+    'imagenes\slider\casasFotos\gallery26.jpg',
+    'imagenes\slider\casasFotos\gallery27.jpg',
+    'imagenes\slider\casasFotos\gallery28.jpg',
+    'imagenes\slider\casasFotos\gallery30.jpg',
+    'imagenes\slider\casasFotos\gallery31.jpg'
 ];
 
-var Casa2LS = [
-    'imagenes\slider\casasLS\gallery32Casa2LS.jpg',
-    'imagenes\slider\casasLS\gallery35.jpg',
-    'imagenes\slider\casasLS\gallery32N.jpg',
-    'imagenes\slider\casasLS\gallery33.jpg',
-    'imagenes\slider\casasLS\gallery34.jpg'
+var Casa2LSfotos = [
+    'imagenes\slider\casasFotos\gallery32Casa2LS.jpg',
+    'imagenes\slider\casasFotos\gallery35.jpg',
+    'imagenes\slider\casasFotos\gallery32N.jpg',
+    'imagenes\slider\casasFotos\gallery33.jpg',
+    'imagenes\slider\casasFotos\gallery34.jpg'
 ];
 
-var Casa3LS = [
-    'imagenes\slider\casasLS\gallery40.jpg',
-    'imagenes\slider\casasLS\gallery32.jpg',
-    'imagenes\slider\casasLS\gallery36.jpg',
-    'imagenes\slider\casasLS\gallery37.jpg',
-    'imagenes\slider\casasLS\gallery38.jpg',
-    'imagenes\slider\casasLS\gallery39.jpg'
+var Casa3LSfotos = [
+    'imagenes\slider\casasFotos\gallery40.jpg',
+    'imagenes\slider\casasFotos\gallery32.jpg',
+    'imagenes\slider\casasFotos\gallery36.jpg',
+    'imagenes\slider\casasFotos\gallery37.jpg',
+    'imagenes\slider\casasFotos\gallery38.jpg',
+    'imagenes\slider\casasFotos\gallery39.jpg'
 ];
 
-var Casa4LS = [
-    'imagenes\slider\casasLS\gallery47.jpg',
-    'imagenes\slider\casasLS\gallery48.jpg',
-    'imagenes\slider\casasLS\gallery42.jpg',
-    'imagenes\slider\casasLS\gallery45.jpg',
-    'imagenes\slider\casasLS\gallery46.jpg',
-    'imagenes\slider\casasLS\gallery41.jpg'
+var Casa4LSfotos = [
+    'imagenes\slider\casasFotos\gallery47.jpg',
+    'imagenes\slider\casasFotos\gallery48.jpg',
+    'imagenes\slider\casasFotos\gallery42.jpg',
+    'imagenes\slider\casasFotos\gallery45.jpg',
+    'imagenes\slider\casasFotos\gallery46.jpg',
+    'imagenes\slider\casasFotos\gallery41.jpg'
 ];
 
-var Depto1LS = [
-    'imagenes\slider\casasLS\gallery50.jpg',
-    'imagenes\slider\casasLS\gallery49.jpg',
-    'imagenes\slider\casasLS\gallery28.jpg'
+var Depto1LSfotos = [
+    'imagenes\slider\casasFotos\gallery50.jpg',
+    'imagenes\slider\casasFotos\gallery49.jpg',
+    'imagenes\slider\casasFotos\gallery28.jpg'
 ]; 
 
-var Casa5LS = [
-    'imagenes\slider\casasLS\gallery51.jpg',
-    'imagenes\slider\casasLS\gallery52.jpg',
-    'imagenes\slider\casasLS\gallery53.jpg'
+var Casa5LSfotos = [
+    'imagenes\slider\casasFotos\gallery51.jpg',
+    'imagenes\slider\casasFotos\gallery52.jpg',
+    'imagenes\slider\casasFotos\gallery53.jpg'
 ];
 
-var Depto2LS = [
-    'imagenes\slider\casasLS\gallery54.jpg',
-    'imagenes\slider\casasLS\gallery55.jpg'
+var Depto2LSfotos = [
+    'imagenes\slider\casasFotos\gallery54.jpg',
+    'imagenes\slider\casasFotos\gallery55.jpg'
 ];
 
-var Mansion1LS = [
-    'imagenes\slider\casasLS\gallery58.jpg',
-    'imagenes\slider\casasLS\gallery59.jpg',
-    'imagenes\slider\casasLS\gallery78.jpg',
-    'imagenes\slider\casasLS\gallery60.jpg',
-    'imagenes\slider\casasLS\gallery61.jpg',
-    'imagenes\slider\casasLS\gallery62.jpg',
-    'imagenes\slider\casasLS\gallery63.jpg',
-    'imagenes\slider\casasLS\gallery64.jpg',
-    'imagenes\slider\casasLS\gallery65.jpg',
-    'imagenes\slider\casasLS\gallery66.jpg',
-    'imagenes\slider\casasLS\gallery67.jpg',
-    'imagenes\slider\casasLS\gallery68.jpg',
-    'imagenes\slider\casasLS\gallery69.jpg',
-    'imagenes\slider\casasLS\gallery70.jpg',
-    'imagenes\slider\casasLS\gallery71.jpg',
-    'imagenes\slider\casasLS\gallery72.jpg',
-    'imagenes\slider\casasLS\gallery73.jpg',
-    'imagenes\slider\casasLS\gallery74.jpg',
-    'imagenes\slider\casasLS\gallery75.jpg',
-    'imagenes\slider\casasLS\gallery76.jpg',
-    'imagenes\slider\casasLS\gallery77.jpg'
+var Mansion1LSfotos = [
+    'imagenes\slider\casasFotos\gallery58.jpg',
+    'imagenes\slider\casasFotos\gallery59.jpg',
+    'imagenes\slider\casasFotos\gallery78.jpg',
+    'imagenes\slider\casasFotos\gallery60.jpg',
+    'imagenes\slider\casasFotos\gallery61.jpg',
+    'imagenes\slider\casasFotos\gallery62.jpg',
+    'imagenes\slider\casasFotos\gallery63.jpg',
+    'imagenes\slider\casasFotos\gallery64.jpg',
+    'imagenes\slider\casasFotos\gallery65.jpg',
+    'imagenes\slider\casasFotos\gallery66.jpg',
+    'imagenes\slider\casasFotos\gallery67.jpg',
+    'imagenes\slider\casasFotos\gallery68.jpg',
+    'imagenes\slider\casasFotos\gallery69.jpg',
+    'imagenes\slider\casasFotos\gallery70.jpg',
+    'imagenes\slider\casasFotos\gallery71.jpg',
+    'imagenes\slider\casasFotos\gallery72.jpg',
+    'imagenes\slider\casasFotos\gallery73.jpg',
+    'imagenes\slider\casasFotos\gallery74.jpg',
+    'imagenes\slider\casasFotos\gallery75.jpg',
+    'imagenes\slider\casasFotos\gallery76.jpg',
+    'imagenes\slider\casasFotos\gallery77.jpg'
 ];
 
-var Mansion2LS = [
-    
+var Mansion2LSfotos = [
+    'imagenes\slider\casasFotos\gallery79.jpg',
+    'imagenes\slider\casasFotos\gallery80.jpg',
+    'imagenes\slider\casasFotos\gallery81.jpg',
+    'imagenes\slider\casasFotos\gallery82.jpg',
+    'imagenes\slider\casasFotos\gallery83.jpg',
+    'imagenes\slider\casasFotos\gallery84.jpg',
+    'imagenes\slider\casasFotos\gallery85.jpg',
+    'imagenes\slider\casasFotos\gallery86.jpg',
+    'imagenes\slider\casasFotos\gallery87.jpg',
+    'imagenes\slider\casasFotos\gallery88.jpg'
 ];
+
+var Casa1SFfotos = [
+    'imagenes\slider\casasFotos\gallery89.jpg',
+    'imagenes\slider\casasFotos\gallery90.jpg',
+    'imagenes\slider\casasFotos\gallery91.jpg',
+    'imagenes\slider\casasFotos\gallery92.jpg',
+    'imagenes\slider\casasFotos\gallery93.jpg'
+];
+
+var Casa2SFfotos = [
+    'imagenes\slider\casasFotos\gallery94.jpg',
+    'imagenes\slider\casasFotos\gallery95.jpg'
+];
+
+var Casa3SFfotos = [
+    'imagenes\slider\casasFotos\gallery101.jpg',
+    'imagenes\slider\casasFotos\gallery102.jpg',
+    'imagenes\slider\casasFotos\gallery103.jpg',
+    'imagenes\slider\casasFotos\gallery104.jpg'
+];
+
+var Depto1SFfotos = [
+    'imagenes\slider\casasFotos\gallery105.jpg',
+    'imagenes\slider\casasFotos\gallery106.jpg',
+    'imagenes\slider\casasFotos\gallery107.jpg'
+];
+
+var Casa4SFfotos = [
+    'imagenes\slider\casasFotos\gallery108.jpg',
+    'imagenes\slider\casasFotos\gallery109.jpg',
+    'imagenes\slider\casasFotos\gallery110.jpg'
+];
+
+var Casa5SFfotos = [
+    'imagenes\slider\casasFotos\gallery111.jpg',
+    'imagenes\slider\casasFotos\gallery112.jpg',
+    'imagenes\slider\casasFotos\gallery83.jpg',
+    'imagenes\slider\casasFotos\gallery84.jpg'
+];
+
+var Casa6SFfotos = [
+    'imagenes\slider\casasFotos\gallery113.jpg',
+    'imagenes\slider\casasFotos\gallery114.jpg',
+    'imagenes\slider\casasFotos\gallery103.jpg',
+];
+
+var Casa1LVfotos = [
+    'imagenes\slider\casasFotos\gallery115.jpg',
+    'imagenes\slider\casasFotos\gallery116.jpg',
+    'imagenes\slider\casasFotos\gallery117.jpg',
+    'imagenes\slider\casasFotos\gallery118.jpg'
+];
+
+var Casa2LVfotos = [
+    'imagenes\slider\casasFotos\gallery119.jpg',
+    'imagenes\slider\casasFotos\gallery120.jpg',
+    'imagenes\slider\casasFotos\gallery121.jpg'
+];
+
+var Casa3LVfotos = [
+    'imagenes\slider\casasFotos\gallery122.jpg',
+    'imagenes\slider\casasFotos\gallery123.jpg',
+    'imagenes\slider\casasFotos\gallery124.jpg',
+    'imagenes\slider\casasFotos\gallery125.jpg',
+    'imagenes\slider\casasFotos\gallery126.jpg',
+    'imagenes\slider\casasFotos\gallery127.jpg',
+    'imagenes\slider\casasFotos\gallery128.jpg'
+];
+
+var Depto1LVfotos = [
+    'imagenes\slider\casasFotos\gallery129.jpg',
+    'imagenes\slider\casasFotos\gallery130.jpg',
+    'imagenes\slider\casasFotos\gallery131.jpg',
+    'imagenes\slider\casasFotos\gallery132.jpg'
+];
+
+var Depto2LVfotos = [
+    'imagenes\slider\casasFotos\gallery133.jpg',
+    'imagenes\slider\casasFotos\gallery134.jpg',
+    'imagenes\slider\casasFotos\gallery114.jpg'
+];
+
+var Depto3LVfotos = [
+    'imagenes\slider\casasFotos\gallery135.jpg',
+    'imagenes\slider\casasFotos\gallery136.jpg',
+    'imagenes\slider\casasFotos\gallery137.jpg',
+    'imagenes\slider\casasFotos\gallery138.jpg',
+];
+
+var Depto4LVfotos = [
+    'imagenes\slider\casasFotos\gallery139.jpg',
+    'imagenes\slider\casasFotos\gallery140.jpg',
+    'imagenes\slider\casasFotos\gallery141.jpg',
+    'imagenes\slider\casasFotos\gallery142.jpg',
+];
+
+var Depto5LVfotos = [
+    'imagenes\slider\casasFotos\gallery143.jpg',
+    'imagenes\slider\casasFotos\gallery144.jpg',
+    'imagenes\slider\casasFotos\gallery145.jpg',
+];
+
+var Casa4LVfotos = [
+    'imagenes\slider\casasFotos\gallery32N.jpg',
+    'imagenes\slider\casasFotos\gallery33.jpg',
+    'imagenes\slider\casasFotos\gallery34.jpg'
+]
 
 // exporto el arreglo para que esté disponible en el otro script
 module.export = imagenes;
--- slidesJS.js
+++ slidesJS.js
@@ -1,5 +1,38 @@
 import imagenes from './imagenes.js';
 
+/* 
+ciudades/pueblos de San Andreas: 
+Red County: Dillimore, Blueberry, Montgomery, Palomino Creek.
+Flint County: Back O' Beyond, Beacon Hill, Easter Bay Chemicals, Fallen Tree, Flint Range.
+Whetstone: Angel Pine, Mount Chiliad.
+Tierra Robada: El Quebrado, Bayside, Aldea Malvada, Las Barrancas, Valle Ocultado
+Bone County: Fort Carson, Las Payasadas, Las Brujas, The Big Ear, Área Restringida, 
+Aeropuerto de Verdant Meadows.
+Las Venturas, San Fierro, Los Santos
+*/
+
+/* barrios de Los Santos:
+Richman, Rodeo, Santa Maria Beach, Marina, Vinewood, Verona Beach, Mulholland, Temple,
+Market, Conference Centre, Verdant Bluffs, Downtown, Cruce de Mulholland, Commerce, 
+Pershing Square, Little Mexico, El Corona, Los Santos Internacional, Glen Park, Idlewood,
+Las Colinas, Jefferson, East Los Santos, Las Flores, East Beach, Ganton, Willowfield, 
+Playa de Seville, Ocean Docks
+*/
+
+/* barrios de San Fierro:
+Doherty, García, Chinatown, Downtown/Financial, Easter Basin, King's, Queens, Missionary Hill,
+City Hall, Santa Flora, Hashbury, Paradiso, Battery Point, Juniper Hill, Juniper Hollow, Ocean Flats,
+Calton Heights, Avispa Country Club, Palisades, Foster Valley, Esplanade North, Esplanade East, 
+Easter Bay Airport
+*/
+
+/* barrios de Las Venturas:
+The Strip, Old Venturas Strip, Blackfield, Pilgrim, The Camel's Toe, Come-A-Lot, Whitewood Estates, 
+The Emerald Isle, Roca Escalante, Redsands West, Rockshore East, Rockshore West, Prickle Pine, Spinybed,
+K.A.C.C. Military Fuels, Creek, Randolph Industrial Estate, Yellow Bell Golf Course, Greenglass College, 
+Aeropuerto Las Venturas, The Pirates In Men's Pants
+*/
+
 let coleccionIMG = [
     'imagenes/slider/paisaje.jpg',
     'imagenes/slider/RE59Mik.jpg',
@@ -8,21 +41,272 @@ let coleccionIMG = [
 
 let casasLS = {
     casa1LS: {
+        localidad: "Los Santos",
         ubicacion: "Verdant Bluffs",
-        tipoProp: "casa",
-        opcion: "comprar",
-        precio: 15000
+        tipoProp: "Casa",
+        opcion: "Alquilar",
+        precio: 355000,
+        moneda: "ARS",
+        imagenes: Casa1LSfotos
+    },
+    Casa2LS: {
+        localidad: "Los Santos",
+        ubicacion: "Willowfield",
+        tipoProp: "Casa",
+        opcion: "Comprar",
+        precio: 15000,
+        moneda: "USD",
+        imagenes: Casa2LSfotos
+    },
+    Casa3LS: {
+        localidad: "Los Santos",
+        ubicacion: "Ganton",
+        tipoProp: "Casa",
+        opcion: "Comprar",
+        precio: 45000,
+        moneda: "USD",
+        imagenes: Casa3LSfotos
+    },
+    Casa4LSfotos: {
+        localidad: "Los Santos",
+        ubicacion: "Jefferson",
+        tipoProp: "Casa",
+        opcion: "Comprar",
+        precio: 15255000,
+        moneda: "ARS",
+        imagenes: Casa4LSfotos
+    },
+    Depto1LS: {
+        localidad: "Los Santos",
+        ubicacion: "Verona Beach",
+        tipoProp: "Departamento",
+        opcion: "Alquilar",
+        precio: 1500,
+        moneda: "USD",
+        imagenes: Depto1LSfotos
+    },
+    Casa5LSfotos: {
+        localidad: "Los Santos",
+        ubicacion: "Santa Maria Beach",
+        tipoProp: "Casa",
+        opcion: "Alquilar",
+        precio: 2500,
+        moneda: "USD",
+        imagenes: Casa5LSfotos
+    },
+    Depto2LS: {
+        localidad: "Los Santos",
+        ubicacion: "Rodeo",
+        tipoProp: "Hotel",
+        opcion: "Alquiler Temporario",
+        precio: 75000,
+        moneda: "ARS",
+        imagenes: Depto2LSfotos
+    },
+    Mansion1LS: {
+        localidad: "Los Santos",
+        ubicacion: "Mulholland",
+        tipoProp: "Casa",
+        opcion: "Comprar",
+        precio: 500000,
+        moneda: "USD",
+        imagenes: Mansion1LSfotos
+    },
+    Mansion2LS: {
+        localidad: "Los Santos",
+        ubicacion: "Mulholland",
+        tipoProp: "Casa",
+        opcion: "Comprar",
+        precio: 55451323,
+        moneda: "ARS",
+        imagenes: Mansion2LSfotos
     }
-    
 };
+
+let casasSF = {
+    Casa1SF: {
+        localidad: "San Fierro",
+        ubicacion: "Doherty",
+        tipoProp: "Casa",
+        opcion: "Comprar",
+        precio: 12150200,
+        moneda: "ARS",
+        imagenes: Casa1SFfotos
+    },
+    Casa2SF: {
+        localidad: "San Fierro",
+        ubicacion: "Doherty",
+        tipoProp: "Casa",
+        opcion: "Comprar",
+        precio: 35000,
+        moneda: "USD",
+        imagenes: Casa2SFfotos
+    },
+    Casa3SF: {
+        localidad: "San Fierro",
+        ubicacion: "Hashbury",
+        tipoProp: "Casa",
+        opcion: "Alquilar",
+        precio: 3500,
+        moneda: "USD",
+        imagenes: Casa3SFfotos
+    },
+    Depto1SF: {
+        localidad: "San Fierro",
+        ubicacion: "Queens",
+        tipoProp: "Hotel",
+        opcion: "Alquiler Temporario",
+        precio: 85120,
+        moneda: "ARS",
+        imagenes: Depto1SFfotos
+    },
+    Casa4SF: {
+        localidad: "San Fierro",
+        ubicacion: "Chinatown",
+        tipoProp: "Casa",
+        opcion: "Alquilar",
+        precio: 122000,
+        moneda: "ARS",
+        imagenes: Casa4SFfotos
+    },
+    Casa5SF: {
+        localidad: "San Fierro",
+        ubicacion: "Calton Heights",
+        tipoProp: "Casa",
+        opcion: "Comprar",
+        precio: 19750100,
+        moneda: "ARS",
+        imagenes: Casa5SFfotos
+    },
+    Casa6SF: {
+        localidad: "San Fierro",
+        ubicacion: "Paradiso",
+        tipoProp: "Casa",
+        opcion: "Alquilar",
+        precio: 1300,
+        moneda: "USD",
+        imagenes: Casa6SFfotos
+    }
+};
+
+let casasLV = {
+    Casa1LV: {
+        localidad: "Las Venturas",
+        ubicacion: "Whitewood Estates",
+        tipoProp: "Casa",
+        opcion: "Alquilar",
+        precio: 1550,
+        moneda: "USD",
+        imagenes: Casa1LVfotos
+    },
+    Casa2LV: {
+        localidad: "Las Venturas",
+        ubicacion: "Aeropuerto Las Venturas",
+        tipoProp: "Casa",
+        opcion: "Comprar",
+        precio: 12500150,
+        moneda: "ARS",
+        imagenes: Casa2LVfotos
+    },
+    Casa3LV: {
+        localidad: "Las Venturas",
+        ubicacion: "Prickle Pine",
+        tipoProp: "Casa",
+        opcion: "Comprar",
+        precio: 2250125,
+        moneda: "USD",
+        imagenes: Casa3LVfotos
+    },
+    Depto1LV: {
+        localidad: "Las Venturas",
+        ubicacion: "Old Venturas Strip",
+        tipoProp: "Hotel",
+        opcion: "Alquilar",
+        precio: 1250,
+        moneda: "USD",
+        imagenes: Depto1LVfotos
+    },
+    Depto2LV: {
+        localidad: "Las Venturas",
+        ubicacion: "Creek",
+        tipoProp: "Hotel",
+        opcion: "Alquiler Temporario",
+        precio: 150,
+        moneda: "USD",
+        imagenes: Depto2LVfotos
+    },
+    Depto3LV: {
+        localidad: "Las Venturas",
+        ubicacion: "The Clown's Pocket",
+        tipoProp: "Hotel",
+        opcion: "Alquiler Temporario",
+        precio: 95000,
+        moneda: "ARS",
+        imagenes: Depto3LVfotos
+    },
+    Depto4LV: {
+        localidad: "Las Venturas",
+        ubicacion: "The Pirates In Men's Pants",
+        tipoProp: "Departamento",
+        opcion: "Alquilar",
+        precio: 750000,
+        moneda: "ARS",
+        imagenes: Depto4LVfotos
+    },
+    Depto5LV: {
+        localidad: "Las Venturas",
+        ubicacion: "The Camel's Toe",
+        tipoProp: "Hotel",
+        opcion: "Alquilar",
+        precio: 525000,
+        moneda: "ARS",
+        imagenes: Depto5LVfotos
+    },
+
+    Casa4LV: {
+        localidad: "Las Venturas",
+        ubicacion: "Rockshore West",
+        tipoProp: "Casa",
+        opcion: "Alquilar",
+        precio: 150000,
+        moneda: "ARS",
+        imagenes: Casa4LVfotos
+    }
+}
+
+const contenedorPropiedades = document.getElementById('vistaPropiedades');
+
+
+/**
+ * crea un div a partir de un elemento del arreglo de coleccion de casas
+ */
+function divProp(propiedad){
+    // creo un nuevo div
+const divPropiedad = document.createElement('div');
+
+
+
+// agrego el nuevo div dentro del contenedor principal
+contenedorPropiedades.appendChild(divPropiedad);
+}
+
+/**
+ * muestra una coleccion de propiedades que cumplen con el filtro
+ */
+function colProps(){
+
+}
+
 
 
 let prevButton = document.getElementById('prev');
 let nextButton = document.getElementById('next');
 
-
 let actualIndex = 0;
 
+/**
+ * cambia la imagen del slide
+*/
 function cambioImagen(action){
     var contenedorIMG = document.querySelector(".img-container");
     if(action === 'next'){
